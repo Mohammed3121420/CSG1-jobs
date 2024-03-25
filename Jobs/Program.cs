1268fcf7363b5add1ffac357607ae4da3e8db51c
Mohammed3121420 <m.mohammed3121420@gmail.com>
2024-03-26 02:26:29 +0300

    rrr

--- Jobs/Program.cs
+++ Jobs/Program.cs
@@ -8,7 +8,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Hote/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
