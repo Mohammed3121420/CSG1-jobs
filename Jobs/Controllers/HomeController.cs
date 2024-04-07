92def48c240d735f16becda1f86250a2a7fa7f10
Mohammed3121420 <m.mohammed3121420@gmail.com>
2024-04-07 05:43:32 +0300

    Layout

--- Jobs/Controllers/HomeController.cs
+++ Jobs/Controllers/HomeController.cs
@@ -1,32 +1,28 @@
-﻿using Jobs.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Jobs.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Contact()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
+        }
+        public IActionResult FindJobs()
+        {
+            return View();
+        }
+        public IActionResult JobsDetails()
+        {
+            return View();
         }
     }
 }
