bba7d49e8b310324e6b7805ee9fdee3f9231b2de
Mohammed3121420 <m.mohammed3121420@gmail.com>
2024-04-07 06:22:21 +0300

    Contact View with database

--- Jobs/Controllers/HomeController.cs
+++ Jobs/Controllers/HomeController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Jobs.Data;
+using Jobs.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Jobs.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly JobContext _db;
+
+        public HomeController(JobContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,9 +21,31 @@ namespace Jobs.Controllers
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Contact()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var contact = new ContactModel()
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Message = model.Message,
+                    Subject = model.Subject,
+                };
+                _db.Contacts.Add(contact);
+                _db.SaveChanges();
+                return RedirectToAction("Contact");
+            }
+            else
+            {
+                return View();
+            }
         }
         public IActionResult FindJobs()
         {
