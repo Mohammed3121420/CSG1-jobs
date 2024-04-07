43f66161bd1d3e9ad44c6ec235348ccb6dd7f701
Mohammed3121420 <m.mohammed3121420@gmail.com>
2024-04-07 05:48:50 +0300

    Revert "Layout"
    This reverts commit 92def48c240d735f16becda1f86250a2a7fa7f10.

--- Jobs/Controllers/HomeController.cs
+++ Jobs/Controllers/HomeController.cs
@@ -1,28 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Jobs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Jobs.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult About()
+
+        public IActionResult Privacy()
         {
             return View();
         }
-        public IActionResult Contact()
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
         {
-            return View();
-        }
-        public IActionResult FindJobs()
-        {
-            return View();
-        }
-        public IActionResult JobsDetails()
-        {
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
