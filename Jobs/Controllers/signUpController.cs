1689511f657dba0936df5aaed7c1029b48242d9f
AliBinMoh1 <alibinmoh1@gmail.com>
2024-04-21 22:54:54 +0300

    tist3

---
+++ Jobs/Controllers/signUpController.cs
@@ -0,0 +1,31 @@
+﻿using Jobs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Jobs.Controllers
+{
+    public class signUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(signUpModel model, string confirmPassword)
+        {
+            if (confirmPassword == model.password)
+            {
+                ViewBag.x = 1;
+                return View();
+            }
+            else
+            {
+                ViewBag.x = 0;
+                return View();
+            }
+
+        }
+
+    }
+}
--- Jobs/obj/Debug/net6.0/Jobs.AssemblyInfo.cs
+++ Jobs/obj/Debug/net6.0/Jobs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jobs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba7d49e8b310324e6b7805ee9fdee3f9231b2de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jobs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jobs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
